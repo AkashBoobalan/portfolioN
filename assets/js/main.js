5f7b775af44faa471a619e5fe91c847292053a0b
AkashBoobalan <akashwar884@gmail.com>
2025-11-20 11:13:00 +0530

    change my DA content

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,7 +24,7 @@ function headerShadow() {
 }
 /* ----- TYPING EFFECT ----- */
 var typingEffect = new Typed(".typedText",{
-  strings : ["Web Developer","Software Engineer","Youtuber"],
+  strings : ["Data Analyst","Web Developer","Software Engineer","Youtuber"],
   loop : true,
   typeSpeed : 100, 
   backSpeed : 80,
